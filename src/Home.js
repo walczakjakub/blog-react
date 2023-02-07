4531de0d36d6995f92810d8b9207fa1786d4d309
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-07 12:19:24 +0100

    test reusing components

--- src/Home.js
+++ src/Home.js
@@ -6,12 +6,13 @@ const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'Kuba Walczak', id: 1},
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Klaas Nowak', id: 2},
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Grzegorz Źdźbło', id: 3}
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kuba Walczak', id: 3}
   ])
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} title="All blogs!"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Kuba Walczak')} title="Kuba's blogs"/>
     </div>
    );
 }
