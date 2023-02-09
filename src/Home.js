b3b1b8ab58efaa6424fa6a5cacb2cef099ecfdde
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-09 11:29:43 +0100

    add loading message

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,25 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
-    fetch(' http://localhost:8000/blogs')
-    .then(res => {
-      return res.json()
-    })
-    .then((data) => {
-      console.log(data)
-      setBlogs(data)
-    })
+    setTimeout(() => {
+        fetch(' http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then((data) => {
+        setBlogs(data)
+        setIsPending(false)
+      })
+    }, 1000)
   }, [])
 
   return ( 
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
    );
