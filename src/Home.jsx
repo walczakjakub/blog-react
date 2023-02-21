71c2d724df5a90cab9d7955d0fe56ebe0a6067ef
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-21 12:59:10 +0100

    test useState hook

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
+  // let name = 'Kuba'
+  const [name, setName] = useState('Kuba')
+  const [age, setAge] = useState(35)
 
-  const handleClickAgain = (name) => {
-    console.log('hello ' + name)
+  const handleClick = () => {
+    setName('Klaas')
+    setAge(36)
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain('Kuba')}>Click me too</button>
     </div>
   );
 }
