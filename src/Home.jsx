34250acffa189d7fd641506965c7c53a1aa62927
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-21 12:46:52 +0100

    test click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name)
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain('Kuba')}>Click me too</button>
     </div>
   );
 }
