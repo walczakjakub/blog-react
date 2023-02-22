66044f804f09bbb0f0e217445dd7380fd5de6184
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-22 12:39:13 +0100

    add separate BlogList component with only one author

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Kuba')} title="Kuba's blogs"/>
     </div>
   );
 }
