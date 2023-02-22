d30d70ac3164974ed8896427772a992fc03fa7f2
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-22 12:48:17 +0100

    add a function deleting blogs

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,10 +8,18 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Kuba', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Kuba')} title="Kuba's blogs"/>
+      <BlogList 
+        blogs={blogs} 
+        title="All blogs" 
+        handleDelete={handleDelete} 
+      />
     </div>
   );
 }
