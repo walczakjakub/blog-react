7ae4026ad3eb1e947c7bf8492301ff78efba7018
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-25 23:13:38 +0100

    add funcionality of adding a post

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,11 +4,28 @@ const Create = () => {
 const [title, setTitle] = useState('')
 const [body, setBody] = useState('')
 const [author, setAuthor] = useState('Kuba')
+const [isPending, setIsPending] = useState(false)
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  const blog = { title, body, author }
+  
+  setIsPending(true)
+
+  fetch('http://localhost:8000/blogs', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(blog)
+  }).then(() => {
+    console.log('new blog added')
+    setIsPending(false)
+  })
+}
 
   return ( 
     <div className="create">
       <h2>Add a new blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text" 
@@ -31,10 +48,8 @@ const [author, setAuthor] = useState('Kuba')
           <option value="Kuba">Kuba</option>
           <option value="Klaas">Klaas</option>
         </select>
-        <button>Add Blog</button>
-        <p>{ title }</p>
-        <p>{ body }</p>
-        <p>{ author }</p>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding...</button>}
       </form>
     </div>
    );
